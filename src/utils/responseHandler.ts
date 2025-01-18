3c1e7ef7ba0e3a76b3b05d5af66b9520e1f9857c
Noman <pxycknomdictator@outlook.com>
2025-01-18 09:44:18 +0500

    added response sender helper class

---
+++ src/utils/responseHandler.ts
@@ -0,0 +1,31 @@
+import { Response } from "../app.js";
+
+export class ResponseHandler {
+  private res: Response;
+  private statusCode: number;
+  private message: string;
+  private status: string;
+  private data?: unknown;
+
+  constructor(
+    res: Response,
+    statusCode: number,
+    message: string,
+    status: string,
+    data?: unknown
+  ) {
+    this.res = res;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.status = status;
+    this.data = data;
+  }
+
+  sendResponse(): void {
+    this.res.status(this.statusCode).json({
+      message: this.message,
+      status: this.status,
+      data: this.data || null,
+    });
+  }
+}
