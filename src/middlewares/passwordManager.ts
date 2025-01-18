a049540211135efa1102d8e36413f5fb203bd632
Noman <pxycknomdictator@outlook.com>
2025-01-18 10:25:42 +0500

    added hash password validator middleware

--- src/middlewares/passwordManager.ts
+++ src/middlewares/passwordManager.ts
@@ -1,6 +1,18 @@
 import * as argon2 from "argon2";
 import { argonOptions } from "../utils/constant.js";
 
-export const passwordGenerator = (password: string): Promise<string> => {
-  return argon2.hash(password, argonOptions);
+interface PasswordPayload {
+  password: string;
+  hash: string;
+}
+
+export const passwordGenerator = async (password: string): Promise<string> => {
+  return await argon2.hash(password, argonOptions);
+};
+
+export const passwordValidator = async ({
+  password,
+  hash,
+}: PasswordPayload): Promise<boolean> => {
+  return await argon2.verify(hash, password);
 };
