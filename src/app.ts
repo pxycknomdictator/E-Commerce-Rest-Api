973768daa35f689692014371979ce5d2fa9aec66
Noman <pxycknomdictator@outlook.com>
2025-01-19 12:21:10 +0500

    import routes in app file

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,20 @@ import { corsOptions, JSON_LIMIT } from "./utils/constant.js";
 
 const app: Express = express();
 
+// Middlewares
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(express.json({ limit: JSON_LIMIT }));
 app.use(express.urlencoded({ extended: false, limit: JSON_LIMIT }));
+
+import userRouter from "./user/user.route.js";
+import packageRouter from "./package/package.route.js";
+import orderRouter from "./order/order.route.js";
+
+// Routes
+app.use("/api/auth", userRouter);
+app.use("/api/", packageRouter);
+app.use("/api/", orderRouter);
 
 export { app, Request, Response, NextFunction };
