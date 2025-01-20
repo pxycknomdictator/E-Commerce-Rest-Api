8964a39d59f94252d7cb7335c057efa61c510da4
Noman <pxycknomdictator@outlook.com>
2025-01-20 15:25:57 +0500

    added authentication middleware for login user

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,4 +3,5 @@ export const config = Object.freeze({
   database_url: process.env.MONGODB_URL as string,
   jwt_secret_key: process.env.JWT_SECRET_KEY as string,
   origin: process.env.ORIGIN as string,
+  url: process.env.URL as string,
 });
--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,15 +1,52 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config/configuration.js";
+import { NextFunction, Request, Response } from "express";
+import { ResponseHandler } from "../utils/responseHandler.js";
 
-interface Payload {
+interface UserPayload {
   _id: string;
   username: string;
   email: string;
 }
 
-export const generateToken = (payload: Payload): string => {
+export const generateToken = (payload: UserPayload): string => {
   return jwt.sign(payload, config.jwt_secret_key as string, {
     algorithm: "HS256",
     expiresIn: "24h",
   });
+};
+
+interface RequestPayload extends Request {
+  user?: UserPayload;
+}
+
+export const authenticationValidator = (
+  req: RequestPayload,
+  res: Response,
+  next: NextFunction
+) => {
+  const token =
+    req.headers.authorization?.split(" ").at(1) || req.cookies.access_token;
+
+  if (token === undefined || token === null) {
+    new ResponseHandler(res, 400, "❌ Unauthorized User", "⚠️ not okay", null);
+    return;
+  }
+
+  try {
+    const isAuthorized = jwt.verify(
+      token,
+      config.jwt_secret_key
+    ) as UserPayload;
+
+    if (!isAuthorized) {
+      new ResponseHandler(res, 400, "⏳ Token is Expired", "⚠️ not okay", null);
+      return;
+    }
+
+    req.user = isAuthorized;
+    next();
+  } catch (error) {
+    new ResponseHandler(res, 404, "🔴 Error", "⚠️ not okay");
+  }
 };
